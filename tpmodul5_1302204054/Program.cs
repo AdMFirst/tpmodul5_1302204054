c8d67112ded0240214fcd4947a624cfb1c2e27a2
Aditya MP <211wadityaw112@gmail.com>
2022-03-23 12:07:33 +0700

    menambahkan class SayaTubeVideo

--- tpmodul5_1302204054/Program.cs
+++ tpmodul5_1302204054/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace com.kpl.tp.modul5
+{
+    
+    public class SayaTubeVideo
+    {
+        protected int id;
+        protected string title;
+        protected int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Random random = new();
+            this.id = random.Next(99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void increasePlayCount(int number)
+        {
+            this.playCount += number;
+        }
+
+        public void printVideoDetails()
+        {
+            Console.WriteLine("ID\t\t:"+this.id.ToString());
+            Console.WriteLine("Title\t\t:"+this.title);
+            Console.WriteLine("Play Count\t:" + this.playCount.ToString());
+        }
+    }
+
+    public class main
+    {
+        public static void Main()
+        {
+            SayaTubeVideo saya = new("Tutorial Design By Contract – Aditya Mardi Pratama.");
+            saya.printVideoDetails();
+            saya.increasePlayCount(1);
+            saya.printVideoDetails();
+        }
+    }
+}
